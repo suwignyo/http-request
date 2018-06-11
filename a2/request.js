49c3d4c63d46c5236c8b1652ffa2b7becbe26ff8
Gerry Suwignyo <gsuwignyo@gmail.com>
2018-06-11 18:16:35 +0000

    re commit, upto npm --save

--- a2/request.js
+++ a2/request.js
@@ -1,11 +1,12 @@
 var request = require('request');
 var fs = require('fs');
 
-request.get('https://sytantris.github.io/http-examples')               // Note 1
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
        .on('error', function (err) {                                   // Note 2
          throw err;
        })
        .on('response', function (response) {                           // Note 3
          console.log('Response Status Code: ', response.statusCode);
+         console.log('Response content-type: ', response.headers['content-type']);
        })
-       .pipe(fs.createWriteStream('./downloaded.html'));               // Note 4
+       .pipe(fs.createWriteStream('./future.jpg'));               // Note 4
