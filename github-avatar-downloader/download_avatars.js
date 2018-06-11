4afb0a4d424a2679b16f453c05e4ef9f78cb974a
Gerry Suwignyo <gsuwignyo@gmail.com>
2018-06-11 21:59:27 +0000

    download avatar needs to take 2 args

--- github-avatar-downloader/download_avatars.js
+++ github-avatar-downloader/download_avatars.js
@@ -6,35 +6,34 @@ var owner = process.argv[2];
 var repo = process.argv[3];
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  if (repoOwner == null || repoName == null){
+  if (repoOwner == null || repoName == null) {
     return null;
+  } else {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': 'token ' + token.GITHUB_TOKEN,
+      }
+    };
+    request(options, function(err, res, body) {
+      var obj = JSON.parse(body);
+      // console.log(obj);
+      cb(err, obj);
+    });
   }
-  else{
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization' : 'token ' + token.GITHUB_TOKEN,
-    }
-  };
-  request(options, function(err, res, body) {
-    var obj = JSON.parse(body);
-    // console.log(obj);
-    cb(err, obj);
-  });
-}
 }
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
-   .on('error', function (err) {
-     throw err;
-   })
-   .on('response', function (response) {
-     console.log('Response Status Code: ', response.statusCode);
-     console.log('Response content-type: ', response.headers['content-type']);
-   })
-   .pipe(fs.createWriteStream(filePath));
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function(response) {
+      console.log('Response Status Code: ', response.statusCode);
+      console.log('Response content-type: ', response.headers['content-type']);
+    })
+    .pipe(fs.createWriteStream(filePath));
 
 }
 
@@ -42,8 +41,8 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 console.log(getRepoContributors(owner, repo, function(err, result) {
   console.log("Errors:", err);
   // var arr = []
-  for (i = 0; i < result.length; i++){
-    downloadImageByURL(result[i].avatar_url,  ('./avatars/' + result[i].login + ".jpg"));
+  for (i = 0; i < result.length; i++) {
+    downloadImageByURL(result[i].avatar_url, ('./avatars/' + result[i].login + ".jpg"));
     // arr.push(result[i].avatar_url); checking output
   }
   // console.log(arr);
