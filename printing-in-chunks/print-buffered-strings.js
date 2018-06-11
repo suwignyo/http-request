a57f8b9fada8dbbbbd7337033cfcc5883716b85b
Gerry Suwignyo <gsuwignyo@gmail.com>
2018-06-11 16:00:27 +0000

    step 1 and 2 done

---
+++ printing-in-chunks/print-buffered-strings.js
@@ -0,0 +1,33 @@
+
+
+var https = require('https');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step1.html'
+};
+
+function getAndPrintHTML () {
+ https.get(requestOptions, function (response) {
+    var body = '';
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+
+
+      for (i = 0; i < data.length; i++){
+        body += data[i];
+      }
+
+    });
+    response.on('end', function() {
+      console.log('Response stream complete.');
+      console.log(body);
+    });
+
+  });
+
+}
+
+
+console.log(getAndPrintHTML());
